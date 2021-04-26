9d89d248ae4644761a93c7bf712412f80aee29d6
Daryl <daryl.9@gmail.com>
2021-04-26 10:55:59 +0800

    Initial Commit

---
+++ SharingDataTry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SharingDataTry
+//
+//  Created by admin on 26/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SharingDataTry/SharingDataTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharingDataTryApp.swift
+//  SharingDataTry
+//
+//  Created by admin on 26/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SharingDataTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
